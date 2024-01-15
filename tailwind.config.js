f976c73b9bb61ef3423c917541b535ca54efbaec
root <marvellousgboun@gmail.com>
2024-01-15 11:42:55 +0100

    keep commiting

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,8 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        mobile:
-          "url('React/ip-adress-tracker/src/assets/images/pattern-bg-mobile.png')",
-        desktop:
-          "url('React/ip-adress-tracker/src/assets/images/pattern-bg-desktop.png')"
+        mobile: "url('./src/assets/images/pattern-bg-mobile.png')",
+        desktop: "url('./src/assets/images/pattern-bg-desktop.png')"
       },
       colors: {
         DarkGray: "hsl(0, 0%, 59%)",
